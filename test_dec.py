95acf4ab77635afaa65468ed9b006ed126732d15
Joseph <josephvincenp2005@gmail.com>
2026-02-24 12:19:56 +0530

    Fix merge conflict in test_dec.py

--- test_dec.py
+++ test_dec.py
@@ -1,9 +1,6 @@
 from app import storage
 
 # Retrieve decrypted
-<<<<<<< HEAD
-retrieved = storage.get_record("patient456")
-=======
 retrieved = storage.get_record("patient123")
->>>>>>> a0036b4867e917ca07ce2b1c346e6c23f8d75649
+
 print("Retrieved record:", retrieved)
